8ea048b764d31d15b68d3bbf238b93359e6d0832
Dominik Swatek <dominik.swatek87@gmail.com>
2019-11-16 18:35:33 +0100

    Add Caliburn Micro

---
+++ WpfAkuSQLiteHome/ViewModels/SBCalculatorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAkuSQLiteHome.ViewModels
+{
+    public class SBCalculatorViewModel : Conductor<object>.Collection.AllActive, ISBCalculatorViewModel
+    {
+
+        public ISetBirthDateViewModel SetBirthDateViewModel { get; }
+        public ISBTableVIewModel SBTableVIewModel { get; }
+
+        public SBCalculatorViewModel(ISetBirthDateViewModel setBirthDateViewModel, ISBTableVIewModel sBTableVIewModel)
+        {
+            SetBirthDateViewModel = setBirthDateViewModel;
+            SBTableVIewModel = sBTableVIewModel;
+        }
+    }
+}
---
+++ WpfAkuSQLiteHome/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAkuSQLiteHome.ViewModels
+{
+    public class ShellViewModel : Conductor<object>.Collection.OneActive
+    {
+        public ISBCalculatorViewModel SBCalculatorViewModel { get; }
+
+        public ShellViewModel(ISBCalculatorViewModel sBCalculatorViewModel)
+        {
+            SBCalculatorViewModel = sBCalculatorViewModel;
+        }
+
+        public void LoadPageOne() => ActivateItem(SBCalculatorViewModel);
+    }
+}
