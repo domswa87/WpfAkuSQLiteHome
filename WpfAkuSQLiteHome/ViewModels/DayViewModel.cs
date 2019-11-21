78cf66b05dfae5967b9455c54214546f9e6b72d2
dominik_87 <dominik.swatek@media-press.tv>
2019-11-21 16:45:39 +0100

    Add conformation windows about event

--- WpfAkuSQLiteHome/ViewModels/DayViewModel.cs
+++ WpfAkuSQLiteHome/ViewModels/DayViewModel.cs
@@ -4,19 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WpfAkuSQLiteHome.ViewModels
 {
     public class DayViewModel : Screen, IDayViewModel
     {
+        private string itemValue;
+        private Visibility visibilityProp = Visibility.Hidden;
+        private Thickness marginDS = new Thickness(50, 20, 0, 0);
+
+        public Thickness MarginDS
+        {
+            get => marginDS; set
+            {
+                marginDS = value;
+                NotifyOfPropertyChange(() => MarginDS);
+            }
+        }
+        public Visibility VisibilityProp
+        {
+            get => visibilityProp;
+            set
+            {
+                visibilityProp = value;
+                NotifyOfPropertyChange(() => VisibilityProp);
+            }
+        }
         public IEventAggregator EventAggregator { get; }
+        public bool IsWindowBox { get; set; } = false;
 
         public DayViewModel(IEventAggregator eventAggregator)
         {
             EventAggregator = eventAggregator;
         }
 
-        private string itemValue;
+
 
         public int ItemIndex { get; set; }
         public string ItemValue
@@ -44,7 +67,41 @@ namespace WpfAkuSQLiteHome.ViewModels
                 ItemValue = "12:00";
 
             EventAggregator.PublishOnUIThread(ItemValue);
+
+            MessageBoxResult result = MessageBox.Show("Do you want to create new event ? \r\n aaa \r\n ddd",
+                                         "Confirmation",
+                                         MessageBoxButton.YesNo,
+                                         MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                VisibilityProp = Visibility.Visible;
+            }
+            else
+            {
+                VisibilityProp = Visibility.Hidden;
+            }
         }
 
+
+        public void CancelDS()
+        {
+            VisibilityProp = Visibility.Hidden;
+            MessageBox.Show("CancelDS");
+        }
+
+        public void OKDS()
+        {
+            VisibilityProp = Visibility.Hidden;
+            MessageBox.Show("Task is created");
+        }
+
+
+        public void marginUP()
+        {
+            double n = MarginDS.Top;
+            n += 10;
+            MarginDS = new Thickness(50, n, 0, 0);
+
+        }
     }
 }
