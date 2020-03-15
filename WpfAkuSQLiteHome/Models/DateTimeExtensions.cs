59678b82f77dad1205a6b8d3c645dd0c5ab4152d
Dominik Swatek <dominik.swatek87@gmail.com>
2020-03-15 10:48:19 +0100

    start

--- WpfAkuSQLiteHome/Models/DateTimeExtensions.cs
+++ WpfAkuSQLiteHome/Models/DateTimeExtensions.cs
@@ -11,7 +11,7 @@ namespace WpfAkuSQLiteHome.Models
         public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
         {
             int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
-            return dt.AddDays(-1 * diff).Date;
+            return dt.AddDays(-1 * diff).Date; //
         }
     }
 }
