4a37edc9681d67d2366f12e190a814ce373b2a7b
Dominik Swatek <dominik.swatek87@gmail.com>
2020-03-17 22:59:03 +0100

    add scroll

--- WpfAkuSQLiteHome/ViewModels/Calculator/sub/TableCalculatorViewModel.cs
+++ WpfAkuSQLiteHome/ViewModels/Calculator/sub/TableCalculatorViewModel.cs
@@ -14,19 +14,6 @@ namespace WpfAkuSQLiteHome.ViewModels
     public class TableCalculatorViewModel : Screen, ITableCalculatorViewModel
     {
         // HOUR
-
-        private string h1;
-        private string h2;
-        private string h3;
-        private string h4;
-        private string h5;
-        private string h6;
-        private string h7;
-        private string h8;
-        private Brush hourGMColor = new SolidColorBrush(Colors.LightGray);
-        private Brush hourSteamColor = new SolidColorBrush(Colors.LightGray);
-        private Brush hourBranchColor = new SolidColorBrush(Colors.LightGray);
-
         public string H1 { get => h1; set { h1 = value; NotifyOfPropertyChange(() => H1); } }
         public string H2 { get => h2; set { h2 = value; NotifyOfPropertyChange(() => H2); } }
         public string H3 { get => h3; set { h3 = value;NotifyOfPropertyChange(() => H3);  } }
@@ -40,19 +27,6 @@ namespace WpfAkuSQLiteHome.ViewModels
         public Brush HourBranchColor { get => hourBranchColor; set { hourBranchColor = value; NotifyOfPropertyChange(() => HourBranchColor); } }
 
         // DAY
-
-        private string d1;
-        private string d2;
-        private string d3;
-        private string d4;
-        private string d5;
-        private string d6;
-        private string d7;
-        private string d8;
-        private Brush dayGMColor = new SolidColorBrush(Colors.LightGray);
-        private Brush daySteamColor = new SolidColorBrush(Colors.LightGray);
-        private Brush dayBranchColor = new SolidColorBrush(Colors.LightGray);
-
         public string D1 { get => d1; set { d1 = value; NotifyOfPropertyChange(() => D1); } }
         public string D2 { get => d2; set { d2 = value; NotifyOfPropertyChange(() => D2); } }
         public string D3 { get => d3; set { d3 = value; NotifyOfPropertyChange(() => D3); } }
@@ -66,19 +40,6 @@ namespace WpfAkuSQLiteHome.ViewModels
         public Brush DayBranchColor { get => dayBranchColor; set { dayBranchColor = value; NotifyOfPropertyChange(() => DayBranchColor); } }
 
         // MONTH
-
-        private string m1;
-        private string m2;
-        private string m3;
-        private string m4;
-        private string m5;
-        private string m6;
-        private string m7;
-        private string m8;
-        private Brush monthGMColor = new SolidColorBrush(Colors.LightGray);
-        private Brush monthSteamColor = new SolidColorBrush(Colors.LightGray);
-        private Brush monthBranchColor = new SolidColorBrush(Colors.LightGray);
-
         public string M1 { get => m1; set { m1 = value; NotifyOfPropertyChange(() => M1); } }
         public string M2 { get => m2; set { m2 = value; NotifyOfPropertyChange(() => M2); } }
         public string M3 { get => m3; set { m3 = value; NotifyOfPropertyChange(() => M3); } }
@@ -92,6 +53,60 @@ namespace WpfAkuSQLiteHome.ViewModels
         public Brush MonthBranchColor { get => monthBranchColor; set { monthBranchColor = value; NotifyOfPropertyChange(() => MonthBranchColor); } }
 
         // YEAR
+        public string Y1 { get => y1; set { y1 = value; NotifyOfPropertyChange(() => Y1); } }
+        public string Y2 { get => y2; set { y2 = value; NotifyOfPropertyChange(() => Y2); } }
+        public string Y3 { get => y3; set { y3 = value; NotifyOfPropertyChange(() => Y3); } }
+        public string Y4 { get => y4; set { y4 = value; NotifyOfPropertyChange(() => Y4); } }
+        public string Y5 { get => y5; set { y5 = value; NotifyOfPropertyChange(() => Y5); } }
+        public string Y6 { get => y6; set { y6 = value; NotifyOfPropertyChange(() => Y6); } }
+        public string Y7 { get => y7; set { y7 = value; NotifyOfPropertyChange(() => Y7); } }
+        public string Y8 { get => y8; set { y8 = value; NotifyOfPropertyChange(() => Y8); } }
+        public Brush YearGMColor { get => yearGMColor; set { yearGMColor = value; NotifyOfPropertyChange(() => YearGMColor); } }
+        public Brush YearSteamColor { get => yearSteamColor; set { yearSteamColor = value; NotifyOfPropertyChange(() => YearSteamColor); } }
+        public Brush YearBranchColor { get => yearBranchColor; set { yearBranchColor = value; NotifyOfPropertyChange(() => YearBranchColor); } }
+
+
+
+
+
+
+
+        private string h1;
+        private string h2;
+        private string h3;
+        private string h4;
+        private string h5;
+        private string h6;
+        private string h7;
+        private string h8;
+        private Brush hourGMColor = new SolidColorBrush(Colors.LightGray);
+        private Brush hourSteamColor = new SolidColorBrush(Colors.LightGray);
+        private Brush hourBranchColor = new SolidColorBrush(Colors.LightGray);
+
+        private string d1;
+        private string d2;
+        private string d3;
+        private string d4;
+        private string d5;
+        private string d6;
+        private string d7;
+        private string d8;
+        private Brush dayGMColor = new SolidColorBrush(Colors.LightGray);
+        private Brush daySteamColor = new SolidColorBrush(Colors.LightGray);
+        private Brush dayBranchColor = new SolidColorBrush(Colors.LightGray);
+
+
+        private string m1;
+        private string m2;
+        private string m3;
+        private string m4;
+        private string m5;
+        private string m6;
+        private string m7;
+        private string m8;
+        private Brush monthGMColor = new SolidColorBrush(Colors.LightGray);
+        private Brush monthSteamColor = new SolidColorBrush(Colors.LightGray);
+        private Brush monthBranchColor = new SolidColorBrush(Colors.LightGray);
 
         private string y1;
         private string y2;
@@ -105,19 +120,6 @@ namespace WpfAkuSQLiteHome.ViewModels
         private Brush yearSteamColor = new SolidColorBrush(Colors.LightGray);
         private Brush yearBranchColor = new SolidColorBrush(Colors.LightGray);
 
-        public string Y1 { get => y1; set { y1 = value; NotifyOfPropertyChange(() => Y1); } }
-        public string Y2 { get => y2; set { y2 = value; NotifyOfPropertyChange(() => Y2); } }
-        public string Y3 { get => y3; set { y3 = value; NotifyOfPropertyChange(() => Y3); } }
-        public string Y4 { get => y4; set { y4 = value; NotifyOfPropertyChange(() => Y4); } }
-        public string Y5 { get => y5; set { y5 = value; NotifyOfPropertyChange(() => Y5); } }
-        public string Y6 { get => y6; set { y6 = value; NotifyOfPropertyChange(() => Y6); } }
-        public string Y7 { get => y7; set { y7 = value; NotifyOfPropertyChange(() => Y7); } }
-        public string Y8 { get => y8; set { y8 = value; NotifyOfPropertyChange(() => Y8); } }
-        public Brush YearGMColor { get => yearGMColor; set { yearGMColor = value; NotifyOfPropertyChange(() => YearGMColor); } }
-        public Brush YearSteamColor { get => yearSteamColor; set { yearSteamColor = value; NotifyOfPropertyChange(() => YearSteamColor); } }
-        public Brush YearBranchColor { get => yearBranchColor; set { yearBranchColor = value; NotifyOfPropertyChange(() => YearBranchColor); } }
-
-     
         public CalculatorOutput CalculatorOutput;
 
         public void FillTableFromCalculatorOutput(CalculatorOutput calculatorOutput)
